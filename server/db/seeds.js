cbe2abe46ca3d7fb68cbac73ae9d2969e8793138
ChristinaL08 <christinalawrance17@gmail.com>
2022-02-04 10:01:20 +0000

    initial commit

---
+++ server/db/seeds.js
@@ -0,0 +1,57 @@
+use match_me;
+db.dropDatabase();
+
+db.animals.insertMany([
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+]);
+
+db.food.insertMany([
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+  {
+    label: "name",
+    link: "URL",
+  },
+]);
+
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('mind_me');
+
+    const animalsCollection = db.collection('animals');
+    const animalsRouter = createRouter(animalsCollection);
+    app.use('/mind-me/animals', animalsRouter);
+
+    const foodCollection = db.collection('food');
+    const foodRouter = createRouter(foodCollection);
+    app.use('/mind-me/food', foodRouter);
+  })
+  .catch(console.err);
+
+app.listen(5000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
