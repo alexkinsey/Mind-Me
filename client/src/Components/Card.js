71ed8d7f0a25edae3428bb8dd11269380c599602
alexkinsey <alexanderkinsey@icloud.com>
2022-02-10 11:10:48 +0000

    basic styling to cards and container

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import ReactCardFlip from 'react-card-flip';
 
+const CardBackground = styled.div`
+  -webkit-box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.25);
+`;
+
 const Card = () => {
-  const[isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
   return (
     <div>
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-        <div>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/Playing_card_club_A.svg" onClick={() => setIsFlipped(!isFlipped)}/>
-        </div>
+        <CardBackground>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/3/36/Playing_card_club_A.svg"
+            onClick={() => setIsFlipped(!isFlipped)}
+          />
+        </CardBackground>
 
-        <div>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/5/57/Playing_card_heart_A.svg" onClick={() => setIsFlipped(!isFlipped)}/>
-        </div>
+        <CardBackground>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/57/Playing_card_heart_A.svg"
+            onClick={() => setIsFlipped(!isFlipped)}
+          />
+        </CardBackground>
       </ReactCardFlip>
     </div>
   );
--- client/src/Containers/CardContainer.js
+++ client/src/Containers/CardContainer.js
@@ -5,12 +5,26 @@ import Card from '../Components/Card';
 
 const Grid = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 1em;
+
+  width: 1200px;
+  margin: auto;
 `;
 
 const CardContainer = () => {
   return (
     <Grid>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
       <Card />
       <Card />
