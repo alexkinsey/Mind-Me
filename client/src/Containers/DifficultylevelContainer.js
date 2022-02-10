e3d9a47596c09f707d603077200a0fcce4af2777
FinnieRyan <80767436+FinnieRyan@users.noreply.github.com>
2022-02-10 15:18:13 +0000

    more buttons

--- client/src/Containers/DifficultylevelContainer.js
+++ client/src/Containers/DifficultylevelContainer.js
@@ -4,6 +4,9 @@ const DifficultyLevelContainer = ({handleDifficultyButton}) => {
   return (
     <>
       <DifficultyLevelsButton handleDifficultyButton={handleDifficultyButton} difficulty={"Easy"}/>
+      <DifficultyLevelsButton handleDifficultyButton={handleDifficultyButton} difficulty={"Medium"}/>
+      <DifficultyLevelsButton handleDifficultyButton={handleDifficultyButton} difficulty={"Hard"}/>
+      <DifficultyLevelsButton handleDifficultyButton={handleDifficultyButton} difficulty={"Too hot to handle!"}/>
     </>
   );
 };
