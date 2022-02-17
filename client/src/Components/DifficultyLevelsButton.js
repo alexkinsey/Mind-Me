6399fb39eae706808d7b177957c3c2ce2cd84a0a
ChristinaL08 <christinalawrance17@gmail.com>
2022-02-17 13:00:14 +0000

    all buttons styled

--- client/src/Components/DifficultyLevelsButton.js
+++ client/src/Components/DifficultyLevelsButton.js
@@ -1,8 +1,45 @@
+import styled from "styled-components";
+
+const StyledDifficultyButton = styled.button `
+
+background-color: salmon;
+  color: black;
+  font-size: 20px;
+  border-radius: 10px;
+  padding: 2rem 4rem;
+  margin: 1rem;
+  cursor: pointer;
+  border: none;
+  outline: none
+  display: flex;
+  text-align: center;
+  margin-top: 15px;
+
+
+  &:hover {
+    background-color: springGreen;
+    color: black;
+    box-shadow: 0 5px 15px rgba(225, 246, 136, .4);
+
+  }
+`;
+
+const DifficultyButtonContainer = styled.div `
+display: flex;
+justify-content: center;
+`;
+
+
+
 const DifficultyLevelsButton = ({handleDifficultyButton, difficulty}) => {
   return (
+    <DifficultyButtonContainer>
     <div>
-      <button onClick={() => handleDifficultyButton(difficulty)}>{difficulty}</button>
+      <StyledDifficultyButton
+       onClick={() => handleDifficultyButton(difficulty)}>{difficulty}
+       </StyledDifficultyButton>
     </div>
+    </DifficultyButtonContainer>
   );
 };
 
