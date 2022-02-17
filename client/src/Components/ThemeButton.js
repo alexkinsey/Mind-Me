35db0cdbb8f85309a9b43b6216c64043ae059df2
alexkinsey <alexanderkinsey@icloud.com>
2022-02-17 16:20:29 +0000

    corrected centering of menu buttons

--- client/src/Components/ThemeButton.js
+++ client/src/Components/ThemeButton.js
@@ -1,45 +1,28 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 const StyledThemeButton = styled.button`
-
-background-color: springGreen;
+  background-color: springGreen;
   color: black;
+  width: 10rem;
   font-size: 16px;
   border-radius: 4px;
   padding: 1rem 1rem;
   margin: 1rem;
   cursor: pointer;
   border: none;
-  outline: none
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  text-align: center;
-
+  outline: none;
 
   &:hover {
     background-color: salmon;
     color: white;
   }
-
-  .div {
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-  }
 `;
 
 const ThemeButton = ({ theme, handleThemeButton }) => {
   return (
-  
     <div>
-      <StyledThemeButton 
-      onClick={() => handleThemeButton(theme)}>{theme}
-      </StyledThemeButton>
-      </div> 
-    
+      <StyledThemeButton onClick={() => handleThemeButton(theme)}>{theme}</StyledThemeButton>
+    </div>
   );
 };
 
--- client/src/Containers/ThemeButtonContainer.js
+++ client/src/Containers/ThemeButtonContainer.js
@@ -1,15 +1,22 @@
+import styled from 'styled-components';
+
 import BackButton from '../Components/BackButton';
 import ThemeButton from '../Components/ThemeButton';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const ThemeButtonContainer = ({ handleThemeButton }) => {
   return (
-    <>
+    <Wrapper>
       <ThemeButton theme={'Animals'} handleThemeButton={handleThemeButton} />
       <ThemeButton theme={'Celebs'} handleThemeButton={handleThemeButton} />
       <ThemeButton theme={'Locations'} handleThemeButton={handleThemeButton} />
       <ThemeButton theme={'Food'} handleThemeButton={handleThemeButton} />
-      <BackButton></BackButton>
-    </>
+    </Wrapper>
   );
 };
 
