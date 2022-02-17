e5b84ed6a3c32c7a05500f14b621edaf9edec854
alexkinsey <alexanderkinsey@icloud.com>
2022-02-17 13:38:04 +0000

    added flipping logic

--- client/src/Containers/GameContainer.js
+++ client/src/Containers/GameContainer.js
@@ -1,42 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CardContainer from './CardContainer';
 import BackButton from '../Components/BackButton';
 
-
 const GameContainer = ({ cardsToDisplay, themeName }) => {
   const [flippedCards, setFlippedCards] = useState(new Array(cardsToDisplay.length).fill(false));
-  const [chosenCard, setChosenCard] = useState({ id: null, label: null });
+  const [chosenCard1, setChosenCard1] = useState({ id: null, label: null });
   const [chosenCard2, setChosenCard2] = useState({ id: null, label: null });
+
+  useEffect(() => {
+    if (chosenCard1.id !== null && chosenCard2.id !== null) {
+      setTimeout(function () {
+        if (chosenCard1.label !== chosenCard2.label) {
+          flippedCards[chosenCard1.id] = false;
+          flippedCards[chosenCard2.id] = false;
+
+          setChosenCard1({ id: null, label: null });
+          setChosenCard2({ id: null, label: null });
+
+          setFlippedCards(flippedCards);
+        }
+      }, 600);
+      setChosenCard1({ id: null, label: null });
+      setChosenCard2({ id: null, label: null });
+    }
+  }, [chosenCard1, chosenCard2, flippedCards]);
 
   if (cardsToDisplay.length < 1) {
     return <p>Loading...</p>;
   }
 
   const onCardClick = (id, label) => {
-    if (chosenCard.id === null) {
-      setChosenCard({ id: id, label: label });
-      flippedCards[id] = true;
-      setFlippedCards(flippedCards);
+    if (chosenCard1.id === null) {
+      setChosenCard1({ id: id, label: label });
     } else {
-      flippedCards[id] = true;
-      setFlippedCards(flippedCards);
-
-      
-
-      if (chosenCard.label === label) {
-        flippedCards[id] = true;
-      } 
-
-      setTimeout(function () {
-        if (chosenCard.label !== label) {
-          flippedCards[id] = false;
-          flippedCards[chosenCard.id] = false;
-        }
-      }, 50);
-
-      setFlippedCards(flippedCards);
-      setChosenCard({ id: null, label: null });
+      setChosenCard2({ id: id, label: label });
     }
+    flippedCards[id] = true;
+    setFlippedCards(flippedCards);
   };
 
   return (
