5498430a5910639e3d3107ac289e73295e6fb10f
MamoonaArshad <mamoona_arshad@hotmail.com>
2022-02-17 13:08:20 +0000

    Merge branch 'Mamoona' into develop

--- client/src/Containers/CardContainer.js
+++ client/src/Containers/CardContainer.js
@@ -4,10 +4,12 @@ import styled from 'styled-components';
 import Card from '../Components/Card';
 
 const Grid = styled.div`
-display: flex;  
-flex-wrap: wrap;  
-width: 400px;  
-height: 300px;
+   display: grid;
+   max-width: 1000px;
+   max-height: flex;
+   grid-template-columns: repeat(4, 1fr);
+   column-gap: 15px;
+   row-gap: 15px;
 `
 ;
 
