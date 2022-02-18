86c38655978d5a9a4f8dc5fb03c208ab59f943ed
alexkinsey <alexanderkinsey@icloud.com>
2022-02-18 16:44:20 +0000

    tweaked button css

--- client/src/Styles/Button.style.jsx
+++ client/src/Styles/Button.style.jsx
@@ -3,18 +3,24 @@ import styled from "styled-components";
 const PrimaryButton = styled.button`
   background-color: transparent;
   color: white;
-  font-size: 20px;
-  border-radius: 10px;
-  padding: 2rem 4rem;
-  border: 2px solid white;
-  text-align: center;
-  width: 16em;
 
-  &:hover {
-    background-color: ${(props) => props.highlightColour};
-    color: white;
+  padding: 2rem 4rem;
+  width: 16em;
+  border: 2px solid white;
+  border-radius: 10px;
+
+  font-size: 20px;
+  text-align: center;
+  
+  :hover {
     cursor: pointer;
+    background-color: ${(props) => props.highlightColour};
     box-shadow: 0 0 15px ${(props) => props.highlightColour};
+  }
+
+  :active {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.3);
   }
 `;
 
