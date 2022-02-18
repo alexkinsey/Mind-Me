685850692e330ded28029911274401d406a5d921
alexkinsey <alexanderkinsey@icloud.com>
2022-02-18 16:44:32 +0000

    small button use case

--- client/src/Components/BackButton.js
+++ client/src/Components/BackButton.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
-import PrimaryButton from '../Styles/Button.style'
+import PrimaryButton from '../Styles/Button.style';
 
 const StyledBackButton = styled(PrimaryButton)`
-  background-color: rgba(0,0,0,0.3);
-  font-size: 2.5em;
-  padding: 1rem 4rem 1.2rem 4rem;
+  background-color: rgba(0, 0, 0, 0.3);
+  font-size: ${(props) => (props.size === 'small' ? '1.5em' : '2.5em')};
+  padding: ${(props) => (props.size === 'small' ? '0.5rem 1rem 0.6rem 1rem' : '1rem 4rem 1.2rem 4rem')};
   font-weight: bold;
   width: auto;
+  border-width: ${(props) => (props.size === 'small' ? '1.5px' : null)};
 
   &:hover {
     box-shadow: none;
@@ -16,10 +17,12 @@ const StyledBackButton = styled(PrimaryButton)`
   }
 `;
 
-const BackButton = ({ handleBackButton }) => {
+const BackButton = ({ size, handleBackButton }) => {
   return (
     <div>
-      <StyledBackButton onClick={() => handleBackButton()}>&#x2190;</StyledBackButton>
+      <StyledBackButton size={size} onClick={() => handleBackButton()}>
+        &#x2190;
+      </StyledBackButton>
     </div>
   );
 };
